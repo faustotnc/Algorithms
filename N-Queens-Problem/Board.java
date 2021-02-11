51cca5cd451cd57484cad7092870faa3b53720c4
Fausto German <fgermanj@gmail.com>
2021-02-10 23:13:46 -0500

    Added N-Queens Problem

---
+++ N-Queens-Problem/Board.java
@@ -0,0 +1,179 @@
+import java.util.Random;
+
+/**
+ * The board in an NxN chess board with N queens.
+ * 
+ * The queens are allowed to move along the rows of their columns (i.e., vertically),
+ * but not horizontally nor diagonally.
+ * 
+ * We are interested in finding a state of the board where none of the queens collide with
+ * each other diagonally or horizontally. We don't care about vertical collisions since the
+ * queens are unique to their columns.
+ * 
+ * @author Fausto German
+ * @version Feb 10th, 2021
+ */
+public class Board {
+    public final int size;
+
+    // The board is represented with a 1-dimensional array, where
+    // the indices represent a queen's column, while the items
+    // represent a queen's row.
+    private int[] board;
+
+    /**
+     * Initializes the board.
+     * @param size The size of the board.
+     */
+    public Board(int size) { 
+        this.size = size;
+        this.board = new int[size];
+        this.reset();
+    }
+
+    /**
+     * Resets the board.
+     * 
+     * @return The current state of the board.
+     */
+    public Board reset() {
+        for (int col = 0; col < size; col++) {
+            // index = column
+            // item = row
+            int row = new Random().nextInt(size);
+            board[col] = row;
+        }
+
+        return this;
+    }
+
+    /**
+     * Returns the current position of a queen.
+     * 
+     * @param queen The queen's whose position will be computed.
+     * @return The queen's position.
+     */
+    public int getCurrentQueenPos(int queen) {
+        return board[queen];
+    }
+
+    /**
+     * Moves a queen to a row position along its column.
+     * 
+     * @param queen The queen to be moved.
+     * @param row The row where the queen will be moved.
+     * @return The current state of the board.
+     */
+    public Board moveQueenTo(int queen, int row) {
+        if (row >= size) {
+            System.out.println("Tried moving queen #" + queen + " to row #" + row + ".");
+            throw new Error("A queen can only be between rows 0 and " + size + ".");
+        }
+        board[queen] = row;
+        return this;
+    }
+
+    /**
+     * Gets the heuristics of a move, without changing the state of the board.
+     * 
+     * @param queen The queen that would be moved.
+     * @param row The row where the queen would be moved.
+     * @return The heuristics of the board if the move is performed.
+     */
+    public int getMoveHeuristics(int queen, int row) {
+        int originalRow = board[queen];
+    
+        int heuristics = this.moveQueenTo(queen, row).getTotalCollisions();
+        this.moveQueenTo(queen, originalRow);
+        
+        return heuristics;
+    }
+
+    /**
+     * Gets the heuristics for the neighboring states of the board.
+     * 
+     * @return The heuristics for the neighboring states of the board.
+     */
+    public int[] getNeighboringHeuristics() {
+        int[] states = new int[(size * size) - size];
+
+        for (int queen = 0; queen < size; queen++) {
+            int qRow = board[queen];
+            
+            for (int nRow = 0; nRow < size; nRow++) {
+                int base = queen * (size - 1);
+                if (nRow < qRow) states[base + nRow] = getMoveHeuristics(queen, nRow);
+                if (nRow > qRow) states[base + (nRow - 1)] = getMoveHeuristics(queen, nRow);
+            }
+        }
+
+        return states;
+    }
+
+    /**
+     * Computes the total number of collisions in the board.
+     * 
+     * @return The total number of collision in the board.
+     */
+    public int getTotalCollisions() {
+        int collisions = 0;
+        for (int q = 0; q < size; q++) {
+            collisions += getQueenCollisions(q);
+        }
+
+        // We divide by 2 because if A collides with B, then B also collides with A.
+        // We only take into account one of the collisions between A and B.
+        return collisions / 2;
+    }
+
+    /**
+     * Gets the collisions count for the current queen.
+     * 
+     * @param queen The queen whose collisions will be computed.
+     * @return The number of collisions for the current queen.
+     */
+    public int getQueenCollisions(int queen) {
+        int collisions = 0;
+        int CurrQueenRow = board[queen];
+
+        for (int j = 0; j < size; j++) {
+            if (queen != j) {
+                int jthQueenRow = board[j];
+                boolean collides1stDiagonal = jthQueenRow == CurrQueenRow - (j - queen);
+                boolean collides2ndDiagonal = jthQueenRow == CurrQueenRow + (j - queen);
+                boolean collidesHorizontal  = jthQueenRow == CurrQueenRow;
+
+                if (collidesHorizontal || collides1stDiagonal || collides2ndDiagonal) {
+                    collisions++;
+                    continue;
+                }
+            }
+        }
+
+        return collisions;
+    }
+
+    /**
+     * Prints the current state of the board.
+     */
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int item = board[j];
+
+                if (i == item) {
+                    System.out.print("\u001b[33;1m1\u001b[0m");
+                } else {
+                    System.out.print(0);
+                }
+
+                if (j == size - 1) {
+                    System.out.print("\n");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+        }
+    }
+
+}
---
+++ N-Queens-Problem/HillClimb.java
@@ -0,0 +1,114 @@
+/**
+ * The hill climbing algorithm looks at the possible paths a program can take,
+ * and takes a small step towards the path that leads to the optimal solution.
+ * 
+ * This particular Hill-Climbing algorithm implements random restarts
+ * (Hill-Climbing with random restarts) if the path taken leads to a local
+ * minima (that is, the output cannot be improved further).
+ * 
+ * @author Fausto German
+ * @version Feb 10th, 2021
+ */
+public class HillClimb {
+    private Board board;
+    private int currentHeuristics;
+    private int resetCount = 0;
+    private int stateChangeCount = 0;
+
+    /**
+     * Initializes the Hill-Climbing algorithm
+     * @param board
+     */
+    public HillClimb(Board board) {
+        this.board = board;
+        currentHeuristics = this.board.getTotalCollisions();
+    }
+
+    /**
+     * Finds a solution using the Hill-Climbing algorithm.
+     * 
+     * @param maxSteps The max number of steps/restarts the algorithm can take.
+     */
+    public void climb(int maxSteps) {
+        boolean isSolved = false;
+        
+        for (int step = 0; step < maxSteps; step++) {
+            // Possible Paths the program can take at this step
+            int[] possiblePaths = board.getNeighboringHeuristics();
+            // Index of the lowest path. We start with the lowest path being
+            // the first item in the list of possible paths, hence 0.
+            int indexOfLowest = 0;
+            // Number of paths found that will result in a lower heuristic
+            int betterPathsCount = 0;
+
+            // Gets the smallest path
+            for (int i = 0; i < possiblePaths.length; i++) {
+                int path = possiblePaths[i];
+
+                if (path < currentHeuristics) betterPathsCount++;
+                if (path < possiblePaths[indexOfLowest]) indexOfLowest = i;
+            }
+
+            // Prints information about the current state
+            System.out.println("\nCurrent Heuristics: " + currentHeuristics);
+            System.out.println("Neighbor States With Lower Heuristics: " + betterPathsCount);
+            System.out.println("Lowest Heuristic Possible: " + ((currentHeuristics == 0) ? "N/A" : possiblePaths[indexOfLowest]));
+            System.out.println("---- Board State ----");
+            board.print();
+
+            // If the current heuristics is zero (i.e, there are no collisions at this step),
+            // we have found a solution, and the loop can be stopped.
+            if (currentHeuristics == 0) {
+                System.out.println("\u001b[34;1m\nSOLUTION FOUND!\u001b[0m");
+                System.out.println("Number of Resets: " + resetCount);
+                System.out.println("Number of State Changes: " + stateChangeCount);
+                isSolved = true;
+                break;
+            } else {
+                // But if not, then we need to either restart the climb, or take a step forward.
+                System.out.print("Next Action: ");
+            }
+
+            // If there are no better paths at this step, then we start the board.
+            if (betterPathsCount == 0) {
+                restartClimb();
+            } else {
+                // If there is a lowest path, then we take a step forward in that direction.
+                stepForward(indexOfLowest);
+            }
+        }
+
+        // If we could not solve the problem with the given number of steps, we throw an error message.
+        if (!isSolved) {
+            String msg = "\n\u001b[31;1m***** A SOLUTION COULD NOT BE FOUND AFTER ";
+            msg += maxSteps + " STEPS.\u001b[0m";
+            System.out.println(msg);
+        }
+    }
+
+    /**
+     * Takes a step forward in the direction of the `lowestIndex` path.
+     * 
+     * @param lowestIndex The path that, if taken, will produce a the lower
+     *                    heuristic than the current heuristic.
+     */
+    private void stepForward(int lowestIndex) {
+        int queen = Math.floorDiv(lowestIndex, board.size - 1);
+        int currentQueensRow = board.getCurrentQueenPos(queen);
+        int offset = lowestIndex - (queen * (board.size - 1));
+        int newRow = (offset >= currentQueensRow) ? offset + 1 : offset;
+
+        System.out.println("Move queen #" + queen + " to row #" + newRow + ".");
+        currentHeuristics = board.moveQueenTo(queen, newRow).getTotalCollisions();
+        stateChangeCount++;
+    }
+
+    /**
+     * Restarts the climb, and starts the process again.
+     */
+    private void restartClimb() {
+        System.out.println("RESTART.");
+        currentHeuristics = this.board.reset().getTotalCollisions();
+        resetCount++;
+    }
+}
