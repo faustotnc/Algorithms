47cb66fd580d7fa838e60ed3745e6e3211cfa6a6
Fausto German <fgermanj@gmail.com>
2021-03-24 10:11:02 -0400

    Fix Indentation in Node.java
    For some reason, my professor decided to use tabs instead of spaces in the Node.java file. This commit fixes that issue by converting the tabs to spaces.

--- A*/Node.java
+++ A*/Node.java
@@ -9,168 +9,168 @@ import javax.swing.JPanel;
  * @ModifiedBy: Fausto German
  */
 public class Node implements Comparable<Node> {
-	private int row, col, f, g, h;
-	private boolean isTraversable = true;
-	private Node parent = null;
-	// The tile associated with this node.
-	private JPanel _tile;
+    private int row, col, f, g, h;
+    private boolean isTraversable = true;
+    private Node parent = null;
+    // The tile associated with this node.
+    private JPanel _tile;
 
-	/**
-	 * A node or tile in the grid environment.
-	 * 
-	 * @param row The row position for this tile.
-	 * @param col The column position for this tile.
-	 */
-	public Node(int row, int col, JPanel tile) {
-		this.row = row;
-		this.col = col;
-		this._tile = tile;
-	}
+    /**
+     * A node or tile in the grid environment.
+     * 
+     * @param row The row position for this tile.
+     * @param col The column position for this tile.
+     */
+    public Node(int row, int col, JPanel tile) {
+        this.row = row;
+        this.col = col;
+        this._tile = tile;
+    }
 
-	/**
-	 * Sets the `F` heuristic value. The f-value represents how well this node leads
-	 * to the end goal.
-	 */
-	private void setF() {
-		f = g + h;
-	}
+    /**
+     * Sets the `F` heuristic value. The f-value represents how well this node leads
+     * to the end goal.
+     */
+    private void setF() {
+        f = g + h;
+    }
 
-	/**
-	 * Sets the `G` heuristic value. The g-value represents the euclidean distance
-	 * between this node and it's parent node. Modifying this value automatically
-	 * updates the f-value.
-	 * 
-	 * @param value The new g-value.
-	 */
-	public void setG(int value) {
-		g = value;
-		this.setF();
-	}
+    /**
+     * Sets the `G` heuristic value. The g-value represents the euclidean distance
+     * between this node and it's parent node. Modifying this value automatically
+     * updates the f-value.
+     * 
+     * @param value The new g-value.
+     */
+    public void setG(int value) {
+        g = value;
+        this.setF();
+    }
 
-	/**
-	 * Sets this node as not traversable.
-	 */
-	public void setNotTraversable() {
-		this.isTraversable = false;
-	}
+    /**
+     * Sets this node as not traversable.
+     */
+    public void setNotTraversable() {
+        this.isTraversable = false;
+    }
 
-	/**
-	 * Sets the `H` heuristic value. The h-value represents the manhattan distance
-	 * between this node and the goal node. Modifying this value automatically
-	 * updates the f-value.
-	 * 
-	 * @param value The new h-value.
-	 */
-	public void setH(int value) {
-		h = value;
-		this.setF();
-	}
+    /**
+     * Sets the `H` heuristic value. The h-value represents the manhattan distance
+     * between this node and the goal node. Modifying this value automatically
+     * updates the f-value.
+     * 
+     * @param value The new h-value.
+     */
+    public void setH(int value) {
+        h = value;
+        this.setF();
+    }
 
-	/**
-	 * Sets the parent node to this node. The parent node is the node used to reach
-	 * this node, forming a shortest path from the start node to the end node.
-	 * 
-	 * @param n The parent node.
-	 */
-	public void setParent(Node n) {
-		parent = n;
-	}
+    /**
+     * Sets the parent node to this node. The parent node is the node used to reach
+     * this node, forming a shortest path from the start node to the end node.
+     * 
+     * @param n The parent node.
+     */
+    public void setParent(Node n) {
+        parent = n;
+    }
 
-	/**
-	 * Gets the f-value for this node.
-	 * 
-	 * @return The f-value for this node.
-	 */
-	public int getF() {
-		return f;
-	}
+    /**
+     * Gets the f-value for this node.
+     * 
+     * @return The f-value for this node.
+     */
+    public int getF() {
+        return f;
+    }
 
-	/**
-	 * Gets the g-value for this node.
-	 * 
-	 * @return The g-value for this node.
-	 */
-	public int getG() {
-		return g;
-	}
+    /**
+     * Gets the g-value for this node.
+     * 
+     * @return The g-value for this node.
+     */
+    public int getG() {
+        return g;
+    }
 
-	/**
-	 * Gets the h-value for this node.
-	 * 
-	 * @return The h-value for this node.
-	 */
-	public int getH() {
-		return h;
-	}
+    /**
+     * Gets the h-value for this node.
+     * 
+     * @return The h-value for this node.
+     */
+    public int getH() {
+        return h;
+    }
 
-	/**
-	 * Gets the parent of this node.
-	 * 
-	 * @return The parent of this node.
-	 */
-	public Node getParent() {
-		return parent;
-	}
+    /**
+     * Gets the parent of this node.
+     * 
+     * @return The parent of this node.
+     */
+    public Node getParent() {
+        return parent;
+    }
 
-	/**
-	 * Gets the row position of this node.
-	 * 
-	 * @return the row position of this node.
-	 */
-	public int getRow() {
-		return row;
-	}
+    /**
+     * Gets the row position of this node.
+     * 
+     * @return the row position of this node.
+     */
+    public int getRow() {
+        return row;
+    }
 
-	/**
-	 * Gets the column position of this node.
-	 * 
-	 * @return the column position of this node.
-	 */
-	public int getCol() {
-		return col;
-	}
+    /**
+     * Gets the column position of this node.
+     * 
+     * @return the column position of this node.
+     */
+    public int getCol() {
+        return col;
+    }
 
-	/**
-	 * Gets the tile associated with this node.
-	 * @return The node's tile.
-	 */
-	public JPanel tile() {
-		return this._tile;
-	}
+    /**
+     * Gets the tile associated with this node.
+     * @return The node's tile.
+     */
+    public JPanel tile() {
+        return this._tile;
+    }
 
-	/**
-	 * Checks whether this node is traversable or nor.
-	 * 
-	 * @return True if the node is traversable; false otherwise.
-	 */
-	public boolean isTraversable() {
-		return isTraversable;
-	}
+    /**
+     * Checks whether this node is traversable or nor.
+     * 
+     * @return True if the node is traversable; false otherwise.
+     */
+    public boolean isTraversable() {
+        return isTraversable;
+    }
 
-	/**
-	 * Checks whether this node equals another node.
-	 * 
-	 * @param node The node to be compared to this node.
-	 * @return True if the nodes are equal; false otherwise.
-	 */
-	public boolean equals(Object node) {
-		Node n = (Node) node;
-		return row == n.getRow() && col == n.getCol();
-	}
+    /**
+     * Checks whether this node equals another node.
+     * 
+     * @param node The node to be compared to this node.
+     * @return True if the nodes are equal; false otherwise.
+     */
+    public boolean equals(Object node) {
+        Node n = (Node) node;
+        return row == n.getRow() && col == n.getCol();
+    }
 
-	/**
-	 * Compares this node against another node by their f-value. The node with the
-	 * lower f-value is considered a smaller node.
-	 */
-	@Override
-	public int compareTo(Node o) {
-		return this.getF() - o.getF();
-	}
+    /**
+     * Compares this node against another node by their f-value. The node with the
+     * lower f-value is considered a smaller node.
+     */
+    @Override
+    public int compareTo(Node o) {
+        return this.getF() - o.getF();
+    }
 
-	/**
-	 * String version of this node.
-	 */
-	public String toString() {
-		return "Node [" + row + ", " + col + "](" + f + ") – G: " + g + " H: " + h;
-	}
+    /**
+     * String version of this node.
+     */
+    public String toString() {
+        return "Node [" + row + ", " + col + "](" + f + ") – G: " + g + " H: " + h;
+    }
 }
